7d792f330191c189e8e013b1e88e0028e09a5a39
ayush-dedhia25 <ayushdedhia25@gmail.com>
2023-03-25 17:15:28 +0530

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 
 function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -15,6 +15,11 @@ function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{!user.isSignedIn ? <SignInButton /> : <SignOutButton />}</div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
